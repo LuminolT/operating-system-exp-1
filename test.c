00dc56d72769ea226d1634ee36be755d681075a2
LuminolT <copper_sulfate@qq.com>
2022-09-22 10:59:50 +0800

    Init: BOOM

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main() {
+    pid_t pid = getpid();
+    // printf("Hello, World!\n");
+    return 0;
+}
