bde3c16e6a9503aed0c6dea5b081ad4130a2b582
LuminolT <copper_sulfate@qq.com>
2022-09-29 10:07:49 +0800

    format: format

--- subexp-1.c
+++ subexp-1.c
@@ -1,8 +1,8 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
-#include<stdlib.h>
+#include <unistd.h>
 // int main() {
 //     pid_t pid;
 //     pid = fork();
@@ -18,23 +18,21 @@
 //     printf("%d %d\n", i, info);
 //     return 0;
 // }
-int main()
-{
-    printf("进程标识：%d\n真实用户标识：%d\n真实组标识: %d\n",getpid(),getuid(),getgid());
+int main() {
+    printf("进程标识：%d\n真实用户标识：%d\n真实组标识: %d\n", getpid(), getuid(), getgid());
     // int x;
     // scanf("%d",&x);
-    int i=fork();
-    if(!i)//是子进程
+    int i = fork();
+    if (!i)  //是子进程
     {
-        //sleep(1);
-        execlp("ls","ls",(char *)0);
+        // sleep(1);
+        execlp("ls", "ls", (char *)0);
         exit(1);
     }
-    int j=wait(0);
+    int j = wait(0);
     printf("Do you want to quit?(Y/N)\n");
     char option;
-    scanf("%c",&option);
-    if(option=='y'||option=='Y')
+    scanf("%c", &option);
+    if (option == 'y' || option == 'Y')
         exit(0);
-    
 }
--- subexp-4.c
+++ subexp-4.c
@@ -1,54 +1,47 @@
 #include <stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-int main()
-{
-    int child_pid1 ,child_pid2,child_pid3;
-    int pid,status;
-    setbuf(stdout,NULL);
-    child_pid1=fork();
+#include <unistd.h>
+int main() {
+    int child_pid1, child_pid2, child_pid3;
+    int pid, status;
+    setbuf(stdout, NULL);
+    child_pid1 = fork();
     /*创建子进程1*/
-    if(child_pid1==0)
-    { 
-        execlp("echo","echo" ,"child process 1",(char *)0);
+    if (child_pid1 == 0) {
+        execlp("echo", "echo", "child process 1", (char *)0);
         perror("exec1 error.\n ");
         exit(1);
     }
-    child_pid2=fork();
+    child_pid2 = fork();
     /*创建子进程2*/
-    if(child_pid2==0)
-    { 
-        execlp("date","date" ,(char *)0);
+    if (child_pid2 == 0) {
+        execlp("date", "date", (char *)0);
         /*子进程2启动其它程序*/
         perror("exec2 error.In ");
-        exit(2); 
+        exit(2);
     }
-    child_pid3=fork();
+    child_pid3 = fork();
     /*创建子进程3*/
-    if(child_pid3==0)
-    { 
-        execlp("ls","ls", (char *)0);
+    if (child_pid3 == 0) {
+        execlp("ls", "ls", (char *)0);
         perror("exec3 error.n ");
         exit(3);
         puts("Parent process is waiting for chile process return!");
     }
-    while((pid=wait(0))!=-1)
-    { 
-        if(child_pid1==pid)
-        /*若子进程1结束*/
-        printf("child process 1 terminated with status %d\n",(status>>8));
-        else
-        {
-            if(child_pid2==pid)
-            /*若子进程2结束*/
-            printf("child process 2 terminated with status %d\n",(status>>8));
-        else
-        { 
-            if(child_pid3==pid)
-            /*若子进程3结束*/
-            printf("child process 3 terminated with status %d\n" ,(status>>8));
-        }
+    while ((pid = wait(0)) != -1) {
+        if (child_pid1 == pid)
+            /*若子进程1结束*/
+            printf("child process 1 terminated with status %d\n", (status >> 8));
+        else {
+            if (child_pid2 == pid)
+                /*若子进程2结束*/
+                printf("child process 2 terminated with status %d\n", (status >> 8));
+            else {
+                if (child_pid3 == pid)
+                    /*若子进程3结束*/
+                    printf("child process 3 terminated with status %d\n", (status >> 8));
+            }
         }
     }
     puts("All child processes terminated.");
